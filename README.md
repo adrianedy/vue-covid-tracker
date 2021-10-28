519aecbdf0c4aed72faf4c902f533413caf46007
Adrian Edy <adrianedypratama@gmail.com>
2021-10-29 00:43:05 +0800

    Add implementation

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # vue-covid-tracker
 
+Simple [Vue](https://vuejs.org/) app with [Tailwind CSS](https://tailwindcss.com/) and [Covid19 API](https://api.covid19api.com/summary).
+
 ## Project setup
 ```
 npm install
---
+++ src/components/DataBoxes.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="grid md:grid-cols-2 gap-4">
+    <!-- Box 1 -->
+    <div class="shadow-md bg-blue-100 p-10 text-center rounded">
+      <h3 class="text-3xl text-blue-900 font-bold mb-4">Cases</h3>
+
+      <div class="text-2xl mb-4">
+        <span class="font-bold">New:</span>
+        {{ numberWithCommas(statistics.NewConfirmed) }}
+      </div>
+      <div class="text-2xl mb-4">
+        <span class="font-bold">Total:</span>
+        {{ numberWithCommas(statistics.TotalConfirmed) }}
+      </div>
+    </div>
+
+    
+    <!-- Box 2 -->
+    <div class="shadow-md bg-blue-200 p-10 text-center rounded">
+      <h3 class="text-3xl text-blue-900 font-bold mb-4">Death</h3>
+
+      <div class="text-2xl mb-4">
+        <span class="font-bold">New:</span>
+        {{ numberWithCommas(statistics.NewDeaths) }}
+      </div>
+      <div class="text-2xl mb-4">
+        <span class="font-bold">Total:</span>
+        {{ numberWithCommas(statistics.TotalDeaths) }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DataBoxes',
+  props: ['statistics'],
+  methods: {
+    numberWithCommas(x) {
+      return x.toLocaleString()
+    }
+  }
+}
+</script>
